f39bc862cf0a63b34cf735f3022be2957456da7a
Lize Wu <retniw72@gmail.com>
2022-08-16 11:23:09 +0800

    2022-0816 SP14、SP15

--- js/customize.js
+++ js/customize.js
@@ -67,6 +67,118 @@ $(function() {
         $('.full_lightbox').fadeOut();
     });
 
+    // 樓層圖 動態sample
+    $('.floorDemo').find('.area').hide();
+    // 101
+    $('.filter-list').find('._101').mouseover(function(event) {
+        $('.floorDemo').find('._101').stop(true, true).fadeIn();
+    });
+    $('.filter-list').find('._101').mouseout(function(event) {
+        $('.floorDemo').find('._101').stop(true, true).fadeOut();
+    });
+
+    // 樓層圖開關
+    $('.floormap-switch').click(function(event) {
+        if($('.floorDemo').hasClass('show')){
+            $('.floormap-switch').text('開啟樓層圖');
+            $('.floorDemo').removeClass('show').hide();
+        }else{
+            $('.floormap-switch').text('關閉樓層圖');
+            $('.floorDemo').addClass('show').show();
+        }
+    });
+
+    // 101A
+    $('.filter-list').find('._101A').mouseover(function(event) {
+        $('.floorDemo').find('._101A').stop(true, true).fadeIn();
+    });
+    $('.filter-list').find('._101A').mouseout(function(event) {
+        $('.floorDemo').find('._101A').stop(true, true).fadeOut();
+    });
+
+    // select 帶入勾選項
+    $(function() {
+        $("#item_1").CreateMultiCheckBox({ width: '100%', defaultText: '會議廳形式', height: '250px' });
+        $("#item_2").CreateMultiCheckBox({ width: '100%', defaultText: '座椅形式', height: '250px' });
+    })
+    $(document).ready(function() {
+        $(document).on("click", ".MultiCheckBox", function() {
+            var detail = $(this).next();
+            detail.show();
+        });
+        $(document).on("click", ".MultiCheckBoxDetailHeader input", function(e) {
+            e.stopPropagation();
+            var hc = $(this).prop("checked");
+            $(this).closest(".MultiCheckBoxDetail").find(".MultiCheckBoxDetailBody input").prop("checked", hc);
+            $(this).closest(".MultiCheckBoxDetail").next().UpdateSelect();
+        });
+        $(document).on("click", ".MultiCheckBoxDetailHeader", function(e) {
+            var inp = $(this).find("input");
+            var chk = inp.prop("checked");
+            inp.prop("checked", !chk);
+            $(this).closest(".MultiCheckBoxDetail").find(".MultiCheckBoxDetailBody input").prop("checked", !chk);
+            $(this).closest(".MultiCheckBoxDetail").next().UpdateSelect();
+        });
+        $(document).on("click", ".MultiCheckBoxDetail .cont input", function(e) {
+            e.stopPropagation();
+            $(this).closest(".MultiCheckBoxDetail").next().UpdateSelect();
+            var val = $(".MultiCheckBoxDetailBody input:checked").length == $(".MultiCheckBoxDetailBody input").length;
+            $(".MultiCheckBoxDetailHeader input").prop("checked", val);
+        });
+        $(document).on("click", ".MultiCheckBoxDetail .cont", function(e) {
+            var inp = $(this).find("input");
+            var chk = inp.prop("checked");
+            inp.prop("checked", !chk);
+            var multiCheckBoxDetail = $(this).closest(".MultiCheckBoxDetail");
+            var multiCheckBoxDetailBody = $(this).closest(".MultiCheckBoxDetailBody");
+            multiCheckBoxDetail.next().UpdateSelect();
+            var val = $(".MultiCheckBoxDetailBody input:checked").length == $(".MultiCheckBoxDetailBody input").length;
+            $(".MultiCheckBoxDetailHeader input").prop("checked", val);
+        });
+        $(document).mouseup(function(e) {
+            var container = $(".MultiCheckBoxDetail");
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
+                container.hide();
+            }
+        });
+    });
+    var defaultMultiCheckBoxOption = {
+        width: "220px",
+        defaultText: "Select Below",
+        height: "200px"
+    };
+    jQuery.fn.extend({
+        CreateMultiCheckBox: function(options) {
+            var localOption = {};
+            localOption.width = options != null && options.width != null && options.width != undefined ? options.width : defaultMultiCheckBoxOption.width;
+            localOption.defaultText = options != null && options.defaultText != null && options.defaultText != undefined ? options.defaultText : defaultMultiCheckBoxOption.defaultText;
+            localOption.height = options != null && options.height != null && options.height != undefined ? options.height : defaultMultiCheckBoxOption.height;
+            this.hide();
+            this.attr("multiple", "multiple");
+            var divSel = $("<div class='MultiCheckBox'>" + localOption.defaultText + "<span class='k-icon k-i-arrow-60-down'><svg aria-hidden='true' focusable='false' data-prefix='fas' data-icon='sort-down' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 320 512' class='svg-inline--fa fa-sort-down fa-w-10 fa-2x'><path fill='currentColor' d='M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41z' class=''></path></svg></span></div>").insertBefore(this);
+            divSel.css({ width: localOption.width });
+            var detail = $("<div class='MultiCheckBoxDetail'><div class='MultiCheckBoxDetailHeader'><input type='checkbox' class='mulinput' value='-1982' /><div>全部</div></div><div class='MultiCheckBoxDetailBody'></div></div>").insertAfter(divSel);
+            detail.css({
+                width: parseInt(options.width) + 10,
+                "max-height": localOption.height
+            });
+            var multiCheckBoxDetailBody = detail.find(".MultiCheckBoxDetailBody");
+            this.find("option").each(function() {
+                var val = $(this).attr("value");
+                if (val == undefined) val = "";
+                multiCheckBoxDetailBody.append("<div class='cont'><div><input type='checkbox' class='mulinput' value='" + val + "' /></div><div>" + $(this).text() + "</div></div>");
+            });
+            multiCheckBoxDetailBody.css("max-height", parseInt($(".MultiCheckBoxDetail").css("max-height")) - 28 + "px");
+        },
+        UpdateSelect: function() {
+            var arr = [];
+            this.prev().find(".mulinput:checked").each(function() {
+                arr.push($(this).val());
+            });
+            this.val(arr);
+        }
+    });
+
 
     // 首頁 卡片輪播
     $('.cardSlider').slick({
